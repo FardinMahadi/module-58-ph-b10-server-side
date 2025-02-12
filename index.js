f82b9b1b1c55b09b9b5c722e2aca81c328565247
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-02-12 10:23:28 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 require("dotenv").config();
 
@@ -36,6 +37,13 @@ async function run() {
     const jobApplicationCollection = client
       .db("jobPortal")
       .collection("jobs_applications");
+
+    // Auth related APIs
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, "secret", { expiresIn: "1h" });
+      res.send(token);
+    });
 
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
